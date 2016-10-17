b0f23a06f74ddfaf00246a1405242f5c7243f555
Jackson Mcglasson <Jackson-McGlasson@users.noreply.github.com>
2016-10-17 14:18:40 -0700

    Worked on milestone 1

--- app/src/main/java/com/academy/app/courtcounter/MainActivity.java
+++ app/src/main/java/com/academy/app/courtcounter/MainActivity.java
@@ -2,7 +2,7 @@ package com.academy.app.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
- 
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
