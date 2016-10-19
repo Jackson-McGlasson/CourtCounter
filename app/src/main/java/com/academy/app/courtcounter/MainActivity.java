6d96741a3392d51b5243b75a20a8e77c5b9d6825
Jackson Mcglasson <Jackson-McGlasson@users.noreply.github.com>
2016-10-19 13:38:11 -0700

    worked on milestone 2 button

--- app/src/main/java/com/academy/app/courtcounter/MainActivity.java
+++ app/src/main/java/com/academy/app/courtcounter/MainActivity.java
@@ -2,12 +2,19 @@ package com.academy.app.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView3;
+    public void onClick1(Button button1){
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView3 = (TextView)findViewById(R.id.textView3);
+        textView3.setText("0");
     }
 }
