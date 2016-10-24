819831bcebe19d697f4fdcacd4a12822f232dc3c
Jackson Mcglasson <Jackson-McGlasson@users.noreply.github.com>
2016-10-24 14:18:07 -0700

    work on milestone 5

--- app/src/main/java/com/academy/app/courtcounter/MainActivity.java
+++ app/src/main/java/com/academy/app/courtcounter/MainActivity.java
@@ -1,5 +1,7 @@
 package com.academy.app.courtcounter;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView3;
     private TextView textView6;
     private int a;
+    public team1Score = String.valueOf(textView6)
     public void onClick1(Button button1){
         a++;
         textView3.setText(String.valueOf(a));
@@ -18,6 +21,22 @@ public class MainActivity extends AppCompatActivity {
         a++;
         textView6.setText(String.valueOf(a));
         textView6.setText("" +a);
+
+    }
+    public void onClick3(Button resetButton){
+        AlertDialog gameOverAlert;
+        gameOverAlert= new AlertDialog.Builder(MainActivity.this).create();
+        gameOverAlert.setTitle("Alert Title");
+        if (team1Score > team2Score) {
+            gameOverAlert.setMessage("Team 1 wins");
+        }
+        gameOverAlert.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.show();
     }
 
     @Override
