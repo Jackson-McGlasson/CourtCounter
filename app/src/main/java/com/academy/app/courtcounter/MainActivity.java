175bc1b6d5befedd1186f67f50abda8b789bf2fb
Jackson Mcglasson <Jackson-McGlasson@users.noreply.github.com>
2016-10-31 13:59:35 -0700

    milestone 6

--- app/src/main/java/com/academy/app/courtcounter/MainActivity.java
+++ app/src/main/java/com/academy/app/courtcounter/MainActivity.java
@@ -36,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
                         dialog.dismiss();
                     }
                 });
+        gameOverAlert.show();
     }
 
     @Override
